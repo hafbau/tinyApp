9dc0c8d05bb72f9d37a79c165c926a4e76549e9d
Hafiz Suara <hafbau@yahoo.com>
2017-01-18 06:40:16 +0000

    login implemented

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
+cookieParser = require('cookie-parser');
 var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,7 +23,7 @@ function formatURL (longU) {
 }
 
 app.get("/", (req, res) => {
-  res.end("Hello!\n");
+  res.end("<html><body>Hello urls <a href='/urls'>urls list</a></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -29,20 +31,25 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+  res.end("<html><body>Hello\n <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, fullURL: urlDatabase[req.params.id] };
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.id,
+    fullURL: urlDatabase[req.params.id] };
+
   res.render("urls_show", templateVars);
 });
 
@@ -53,7 +60,7 @@ app.post("/urls", (req, res) => {
   longU = formatURL(longU);
 
   urlDatabase[shortU] = longU;
-  res.redirect(`http://localhost:8080/urls/${shortU}`);
+  res.redirect(`/urls/${shortU}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -77,7 +84,17 @@ app.post("/urls/:shortURL", (req, res) => {
 
   urlDatabase[shortU] = formatURL(req.body.longURL);
   res.redirect(`/urls/${shortU}`);
-})
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
